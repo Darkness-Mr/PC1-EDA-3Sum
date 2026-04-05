f0b00829b88677673b3ca2fc202ea7df0d676ecf
Marco HB <marco@iMac.local>
2026-04-05 13:51:27 -0500

    Creamos nuestra funcion principal que inicializa el programa, creamos el objeto solution y un caso de prueba

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 #include "solution.hpp"
+
+int main(){
+    std::cout << "==========================\n";
+    std::cout << "Iniciando practica de 3Sum\n";
+
+    Solution solution;
+
+    std::vector<int> prueba = {-1, 0, 1, 2, -1, -4};
+
+    return 0;
+}
