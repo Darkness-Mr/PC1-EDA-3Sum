a2c0ff96e6dee1b59ba1c5af9fd530a02b4c6204
Marco HB <marco@iMac.local>
2026-04-05 13:45:08 -0500

    Le decimos que existe una funcion en la clase Solution, con ello creamos el vector que almacenara nuestras respuesta, y lo devolvemos

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,2 +1,7 @@
 #include <solution.hpp>
 #include <algorithm>
+
+std::vector<std::vector<int>> Solution::threeSum(std::vector<int>& nums){
+    std::vector<std::vector<int>> result;
+    return result;
+}
