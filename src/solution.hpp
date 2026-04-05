9cabcafa7910f0c7c5d69f2905624b03e6861b96
Marco HB <marco@iMac.local>
2026-04-05 14:18:17 -0500

    Defino la solución con fuerza bruta como threeSumBrute

--- src/solution.hpp
+++ src/solution.hpp
@@ -5,7 +5,11 @@
 
 class Solution {
     public:
+        // Solución óptima esperada: O(n^2)
         std::vector<std::vector<int>> threeSum(std::vector<int>& nums);
+
+        // Solución de fuerza bruta para el benchmark: O(n^3)
+        std::vector<std::vector<int>> threeSumBruteForce(std::vector<int>& nums);
 };
 
 #endif
