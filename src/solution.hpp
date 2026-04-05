1037dd6bbc6d5949b9b9793de09463c7ca85a8ba
Marco HB <marco@iMac.local>
2026-04-05 14:19:06 -0500

    Le cambio de nombre de threeSumBruteForce a threeSumBrute

--- src/solution.hpp
+++ src/solution.hpp
@@ -8,8 +8,8 @@ class Solution {
         // Solución óptima esperada: O(n^2)
         std::vector<std::vector<int>> threeSum(std::vector<int>& nums);
 
-        // Solución de fuerza bruta para el benchmark: O(n^3)
-        std::vector<std::vector<int>> threeSumBruteForce(std::vector<int>& nums);
+        // Solución bruta para el benchmark: O(n^3)
+        std::vector<std::vector<int>> threeSumBrute(std::vector<int>& nums);
 };
 
 #endif
